e896c3ebaf39f1680a4f95d375f88522675911d2
蔡文 <wen.cai@microcloudsys.com>
2025-01-02 17:59:47 +0800

    feat: 删除 Command palette 命令面板不需要的功能5个入口

--- excalidraw-app/App.tsx
+++ excalidraw-app/App.tsx
@@ -987,81 +987,84 @@ const ExcalidrawWrapper = () => {
                 setShareDialogState({ isOpen: true, type: "share" });
               },
             },
-            {
-              label: "GitHub",
-              icon: GithubIcon,
-              category: DEFAULT_CATEGORIES.links,
-              predicate: true,
-              keywords: [
-                "issues",
-                "bugs",
-                "requests",
-                "report",
-                "features",
-                "social",
-                "community",
-              ],
-              perform: () => {
-                window.open(
-                  "https://github.com/excalidraw/excalidraw",
-                  "_blank",
-                  "noopener noreferrer",
-                );
-              },
-            },
-            {
-              label: t("labels.followUs"),
-              icon: XBrandIcon,
-              category: DEFAULT_CATEGORIES.links,
-              predicate: true,
-              keywords: ["twitter", "contact", "social", "community"],
-              perform: () => {
-                window.open(
-                  "https://x.com/excalidraw",
-                  "_blank",
-                  "noopener noreferrer",
-                );
-              },
-            },
-            {
-              label: t("labels.discordChat"),
-              category: DEFAULT_CATEGORIES.links,
-              predicate: true,
-              icon: DiscordIcon,
-              keywords: [
-                "chat",
-                "talk",
-                "contact",
-                "bugs",
-                "requests",
-                "report",
-                "feedback",
-                "suggestions",
-                "social",
-                "community",
-              ],
-              perform: () => {
-                window.open(
-                  "https://discord.gg/UexuTaE",
-                  "_blank",
-                  "noopener noreferrer",
-                );
-              },
-            },
-            {
-              label: "YouTube",
-              icon: youtubeIcon,
-              category: DEFAULT_CATEGORIES.links,
-              predicate: true,
-              keywords: ["features", "tutorials", "howto", "help", "community"],
-              perform: () => {
-                window.open(
-                  "https://youtube.com/@excalidraw",
-                  "_blank",
-                  "noopener noreferrer",
-                );
-              },
-            },
+            /* 删除 Command palette 命令面板不需要的功能入口 start */
+            // {
+            //   label: "GitHub",
+            //   icon: GithubIcon,
+            //   category: DEFAULT_CATEGORIES.links,
+            //   predicate: true,
+            //   keywords: [
+            //     "issues",
+            //     "bugs",
+            //     "requests",
+            //     "report",
+            //     "features",
+            //     "social",
+            //     "community",
+            //   ],
+            //   perform: () => {
+            //     window.open(
+            //       "https://github.com/excalidraw/excalidraw",
+            //       "_blank",
+            //       "noopener noreferrer",
+            //     );
+            //   },
+            // },
+            // {
+            //   label: t("labels.followUs"),
+            //   icon: XBrandIcon,
+            //   category: DEFAULT_CATEGORIES.links,
+            //   predicate: true,
+            //   keywords: ["twitter", "contact", "social", "community"],
+            //   perform: () => {
+            //     window.open(
+            //       "https://x.com/excalidraw",
+            //       "_blank",
+            //       "noopener noreferrer",
+            //     );
+            //   },
+            // },
+            // {
+            //   label: t("labels.discordChat"),
+            //   category: DEFAULT_CATEGORIES.links,
+            //   predicate: true,
+            //   icon: DiscordIcon,
+            //   keywords: [
+            //     "chat",
+            //     "talk",
+            //     "contact",
+            //     "bugs",
+            //     "requests",
+            //     "report",
+            //     "feedback",
+            //     "suggestions",
+            //     "social",
+            //     "community",
+            //   ],
+            //   perform: () => {
+            //     window.open(
+            //       "https://discord.gg/UexuTaE",
+            //       "_blank",
+            //       "noopener noreferrer",
+            //     );
+            //   },
+            // },
+            // {
+            //   label: "YouTube",
+            //   icon: youtubeIcon,
+            //   category: DEFAULT_CATEGORIES.links,
+            //   predicate: true,
+            //   keywords: ["features", "tutorials", "howto", "help", "community"],
+            //   perform: () => {
+            //     window.open(
+            //       "https://youtube.com/@excalidraw",
+            //       "_blank",
+            //       "noopener noreferrer",
+            //     );
+            //   },
+            // },
+            /* 删除 Command palette 命令面板不需要的功能入口 end */
+
             ...(isExcalidrawPlusSignedUser
               ? [
                   {
@@ -1069,7 +1072,7 @@ const ExcalidrawWrapper = () => {
                     label: "Sign in / Go to Excalidraw+",
                   },
                 ]
-              : [ExcalidrawPlusCommand, ExcalidrawPlusAppCommand]),
+              : [ExcalidrawPlusAppCommand]), // 删除 Command palette 命令面板不需要的功能入口：excalidraw+
 
             {
               label: t("overwriteConfirm.action.excalidrawPlus.button"),
